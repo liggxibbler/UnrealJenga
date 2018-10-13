eee10cf37562c0ca58376afcecd13c7b2dcf1d57
Afsaneh <Afsaneh>
2018-10-13 19:27:51 +0330

    Initial commit

---
+++ Source/Jenga/JengaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JengaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JENGA_API AJengaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/Jenga/MyPawn.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyPawn.h"
+#include "GameFramework/Pawn.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CreateDefaultSubobject<UFloatingPawnMovement>("FloatingPawn");
+
+	m_cubeMesh = CreateDefaultSubobject<UStaticMeshComponent>("Visual");
+	m_camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMyPawn::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMyPawn::MoveRight);
+
+}
+
+void AMyPawn::MoveForward(float value)
+{
+	AddMovementInput(GetActorForwardVector(), value);
+}
+
+void AMyPawn::MoveRight(float value)
+{
+	AddMovementInput(GetActorRightVector(), value);
+}
+
